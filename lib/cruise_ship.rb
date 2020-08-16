e0afa3436c3055d244932b1002f89fadcaaa317b
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-16 17:08:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   winner = ""
-  passangers.each do |suit, name|
+  passangers.each do |suite, name|
     if suite == :suite_a && name.start_with? ("A")
       winner = name
     end
